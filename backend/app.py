8588a47c7e97ac943b2e7aa1a7d12e991614ccd4
Dev Patmase <devpatmase1@gmail.com>
2026-06-14 22:59:40 +0530

    Remove uploaded PDFs from repository

--- backend/app.py
+++ backend/app.py
@@ -419,7 +419,8 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(
         host="0.0.0.0",
-        port=5000
+        port=port
     )
